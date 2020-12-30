15873ea6a9f82931e2f18cf63a2cdd375306ca0c
Le Dinh Hieu <wanderlust252@gmail.com>
2020-12-30 13:21:11 +0700

    API CCDC

--- Controllers/CCDCController.cs
+++ Controllers/CCDCController.cs
@@ -68,6 +68,59 @@ namespace ECP_WEBAPI.Controllers
 
             return entityBuilder.ToString();
         }
+        [Route("NhomThietBi")]
+        [HttpGet]
+        public async Task<object> getNhomThietBi(string dbname)
+        { 
+            try
+            {
+
+                using (SqlConnection db = new SqlConnection(GetConnectString(dbname)))
+                {
+                    string query = @"select * from NhomThietBi";
+
+                    return (await db.QueryAsync<object>(query, new { })).ToList();
+                }
+            }
+            catch (Exception ex) { }
+            return null;
+        }
+        [Route("CCDCTrangThai")]
+        [HttpGet]
+        public async Task<object> getCCDCTrangThai(string dbname)
+        {
+            //List<CongCuDungCuAnToanModel> lstData = new List<CongCuDungCuAnToanModel>();
+            try
+            {
+
+                using (SqlConnection db = new SqlConnection(GetConnectString(dbname)))
+                {
+                    string query = @"select * from CCDC_TrangThai";
+
+                    return (await db.QueryAsync<object>(query, new { })).ToList();
+                }
+            }
+            catch (Exception ex) { }
+            return null;
+        }
+        [Route("LoaiThietBi")]
+        [HttpGet]
+        public async Task<object> getLoaiThietBi(string MaNhom,string dbname)
+        {
+            //List<CongCuDungCuAnToanModel> lstData = new List<CongCuDungCuAnToanModel>();
+            try
+            {
+
+                using (SqlConnection db = new SqlConnection(GetConnectString(dbname)))
+                {
+                    string query = @"select * from LoaiThietBi where MaNhom=@MaNhom";
+
+                    return (await db.QueryAsync<object>(query, new { MaNhom })).ToList();
+                }
+            }
+            catch (Exception ex) { }
+            return null;
+        }
         [Route("SoKiemDinh")]
         [HttpGet]
         public async Task<object> getListSoKiemDinh( string CongCuId, string dbname)
@@ -95,6 +148,164 @@ namespace ECP_WEBAPI.Controllers
             catch (Exception ex) { }
             return lstData;
         }
+        [Route("CCDCListCounter")]
+        [HttpGet]
+        public object CCDCListCounter(string filter,
+            string DonViId, string PhongBanId, string TrangThai, string MaLoai, string TrangThaiKiemDinh, string MaNhom, string MaTT,string dbname
+            )
+        {
+            if (filter == "null")
+                filter = "";
+            if (DonViId == "null")
+                DonViId = "";
+            if (PhongBanId == "null")
+                PhongBanId = "";
+            if (TrangThai == "null")
+                TrangThai = "";
+            if (MaLoai == "null")
+                MaLoai = "";
+            if (TrangThaiKiemDinh == "null")
+                TrangThaiKiemDinh = "";
+            if (MaNhom == "null")
+                MaNhom = "";
+            if (MaTT == "null")
+                MaTT = "";
+            string start1 = "";
+            string end1 = "";
+
+            //if (string.IsNullOrEmpty(TuNgay))
+            //{
+
+            //}
+            //else
+            //{
+            //    DateTime dts = DateTime.ParseExact(TuNgay, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            //    start1 = string.Format("{0:yyyy-MM-dd HH:mm:ss}", dts.Date);
+            //}
+
+            //if (string.IsNullOrEmpty(DenNgay))
+            //{
+
+            //}
+            //else
+            //{
+            //    DateTime dte = DateTime.ParseExact(DenNgay, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            //    end1 = string.Format("{0:yyyy-MM-dd HH:mm:ss}", dte.Date);
+            //}
+
+            int count = 0;
+            try
+            {
+
+                using (SqlConnection db = new SqlConnection(GetConnectString(dbname)))
+                {
+                    string query = "select count(ID) " +
+                        "from ThietBiATLD tb " +
+                        "where " +
+                        "((tb.TenThietBi like REPLACE(N'%'+(N'" + filter + "')+N'%',' ',N'%') or N'" + filter + "'=N'') " +
+                        "or (tb.MaHieu like REPLACE(N'%'+(N'" + filter + "')+N'%',' ',N'%') or N'" + filter + "'=N'')) " +
+                        "and (tb.DonViId=@DonViId or @DonViId='') " +
+                        "and (tb.PhongBanID=@PhongBanId or @PhongBanId='') " +
+                        //"and (CONVERT(date,tb.NgayTao) >= CONVERT(date,@TuNgay) or @TuNgay='') " +
+                        //"and (CONVERT(date,tb.NgayTao) <= CONVERT(date,@DenNgay) or @DenNgay='') " +
+                        "and isnull(tb.IsDelete,0)=0 " +
+                        "and tb.MaNhom=@MaNhom " +
+                        "and (tb.MaLoai=@MaLoai or @MaLoai='') " +
+                        "and (tb.MaTT=@MaTT or @MaTT='') "
+                        ;
+
+                    // het han
+                    if (TrangThai == "hh")
+                    {
+                        query = query + "and ( " +
+                            "DATEDIFF(d,(select top(1) NgayKiemTraTiepTheo from SoTheoDoiCCDCAT where MaTB=tb.ID order by ID desc),getdate()) >0 " +
+                            ") "
+                            ;
+                    }
+                    //sap het han 15 ngay
+                    else if (TrangThai == "shh15")
+                    {
+                        query = query + "and ( " +
+                            "DATEDIFF(d,getdate(),(select top(1) NgayKiemTraTiepTheo from SoTheoDoiCCDCAT where MaTB=tb.ID order by ID desc)) <=15 " +
+                            "and DATEDIFF(d,getdate(),(select top(1) NgayKiemTraTiepTheo from SoTheoDoiCCDCAT where MaTB=tb.ID order by ID desc)) >=0 " +
+                            ") "
+                            ;
+                    }
+                    //sap het han 30 ngay
+                    else if (TrangThai == "shh30")
+                    {
+                        query = query + "and ( " +
+                            "DATEDIFF(d,getdate(),(select top(1) NgayKiemTraTiepTheo from SoTheoDoiCCDCAT where MaTB=tb.ID order by ID desc)) <=30 " +
+                            "and DATEDIFF(d,getdate(),(select top(1) NgayKiemTraTiepTheo from SoTheoDoiCCDCAT where MaTB=tb.ID order by ID desc)) >15 " +
+                            ") "
+                            ;
+                    }
+                    //chua het han
+                    else if (TrangThai == "cdh")
+                    {
+                        query = query + "and ( " +
+                            "DATEDIFF(d,getdate(),(select top(1) NgayKiemTraTiepTheo from SoTheoDoiCCDCAT where MaTB=tb.ID order by ID desc)) >30 " +
+                            ") "
+                            ;
+                    }
+
+                    //trang thai kiem dinh dat
+                    if (TrangThaiKiemDinh == "1")
+                    {
+                        query = query + "and ( " +
+                            "(select top(1) isnull(KetQua,0) from SoTheoDoiCCDCAT where MaTB=tb.ID order by ID desc) =1 " +
+                            ") "
+                            ;
+                    }
+                    //trang thai kiem dinh khong dat
+                    else if (TrangThaiKiemDinh == "2")
+                    {
+                        query = query + "and ( " +
+                            "(select top(1) isnull(KetQua,0) from SoTheoDoiCCDCAT where MaTB=tb.ID order by ID desc) =0 " +
+                            ") "
+                            ;
+                    }
+                    //ko co kiem dinh
+                    else if (TrangThaiKiemDinh == "3")
+                    {
+                        query = query + "and ( " +
+                            "(select count(ID) from SoTheoDoiCCDCAT where MaTB=tb.ID) =0 " +
+                            ") "
+                            ;
+                    }
+
+
+                    using (var multipleresult = db.QueryMultiple(query,
+                        new
+                        {
+                            filter = DonViId,
+                            //TuNgay = start1,
+                            //DenNgay = end1,
+                            PhongBanId = PhongBanId,
+                            DonViId = DonViId,
+                            MaLoai = MaLoai,
+                            MaNhom = MaNhom,
+                            MaTT = MaTT
+                        }))
+                    {
+                        try
+                        {
+                            var q = multipleresult.Read<int>();
+                            count = q.FirstOrDefault();
+                        }
+                        catch (Exception ex)
+                        {
+                            count = 0;
+                        }
+
+
+                    }
+                }
+            }
+            catch (Exception ex) { }
+            return count;
+        }
+
         [Route("CCDC")]
         [HttpGet]
         public object getListCongCu(int page, int pageSize, string filter,
